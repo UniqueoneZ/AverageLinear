8d99fe33424cbd86f1640bbbf18df3f53fa9327a
UniqueoneZ <124648060+UniqueoneZ@users.noreply.github.com>
2025-01-03 19:49:51 +0800

    Add files via upload

---
+++ scripts/AverageLinear/light_electricity.sh
@@ -0,0 +1,44 @@
+if [ ! -d "./logs" ]; then
+    mkdir ./logs
+fi
+
+if [ ! -d "./logs/LongForecasting" ]; then
+    mkdir ./logs/LongForecasting
+fi
+model_name=LightAverageLinear_electricity
+
+root_path_name=./dataset/
+data_path_name=electricity.csv
+model_id_name=Electricity
+data_name=custom
+
+
+seq_len=96
+for pred_len in 96 192 336 720
+do 
+    python -u run_longExp.py \
+      --is_training 1 \
+      --root_path $root_path_name \
+      --data_path $data_path_name \
+      --model_id $model_id_name'_'$seq_len'_'$pred_len \
+      --model $model_name \
+      --data $data_name \
+      --features M \
+      --seq_len $seq_len \
+      --pred_len $pred_len \
+      --seg_len 24 \
+      --enc_in 321 \
+      --d_model 256 \
+      --dropout 0.02 \
+      --train_epochs 10 \
+      --patience 3 \
+      --rnn_type gru \
+      --dec_way pmf \
+      --channel_id 1 \
+      --loss mse \
+      --c_layers 3 \
+      --revin 1 \
+      --itr 1 --batch_size 128 --learning_rate 0.001 > logs/LongForecasting/$model_name'_'$model_id_name'_'$seq_len'_'$pred_len.log
+done
+
+
