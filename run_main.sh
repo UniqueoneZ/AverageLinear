6dd95d878cd307ebf879e78d6972b71e9cc1fef7
UniqueoneZ <124648060+UniqueoneZ@users.noreply.github.com>
2025-04-02 14:50:49 +0800

    Delete run_main.sh

--- run_main.sh
+++
@@ -1,7 +0,0 @@
-sh scripts/AverageLinear/etth1.sh;
-sh scripts/AverageLinear/etth2.sh;
-sh scripts/AverageLinear/ettm1.sh;
-sh scripts/AverageLinear/ettm2.sh;
-sh scripts/AverageLinear/weather.sh;
-sh scripts/AverageLinear/electricity.sh;
-sh scripts/AverageLinear/traffic.sh;
